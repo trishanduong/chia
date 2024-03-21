182097116d24b9616c1b98920b9172e8c1e72fcf
trisha <trishanduong@gmail.com>
2024-03-21 00:00:03 -0700

    Initial Commit

---
+++ chia/chiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  chiaApp.swift
+//  chia
+//
+//  Created by trisha on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct chiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
